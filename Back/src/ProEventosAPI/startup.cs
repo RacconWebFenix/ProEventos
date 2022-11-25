88df047b552a6b66cc6c9637b10616c7c2f19710
Marcos César Domingues <dfenixweb@gmail.com>
2022-11-25 17:49:17 -0300

    section 3 introduction dotnet ef sql server

--- Back/src/ProEventosAPI/startup.cs
+++ Back/src/ProEventosAPI/startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ProEventosAPI.Data;
 
 namespace ProEventos.API
 {
@@ -22,11 +24,13 @@ namespace ProEventos.API
         }
 
         public IConfiguration Configuration { get; }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(
+                context => context.UseSqlServer(Configuration.GetConnectionString("Default"))
+            );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
