1d6dc555e40600b1a91f169defb43b229a413a77
Marcos César Domingues <dfenixweb@gmail.com>
2022-11-26 11:24:52 -0300

    section 3 (angular conection server botstrap)

--- Back/src/ProEventosAPI/startup.cs
+++ Back/src/ProEventosAPI/startup.cs
@@ -32,10 +32,11 @@ namespace ProEventos.API
                 context => context.UseSqlServer(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
-            });
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
+});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +54,10 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(cors => cors.AllowAnyHeader()
+            .AllowAnyMethod().AllowAnyOrigin()
+            );
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Front/ProEventosAPP/src/app/eventos/eventos.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss'],
+})
+export class EventosComponent implements OnInit {
+  public eventos: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:7134/api/eventos').subscribe(
+      (res) => (this.eventos = res),
+      (err) => console.log(err)
+    );
+  }
+}
