334c7dc5f57865462a6c66cb474b4bfb7111121a
Marcos César Domingues <dfenixweb@gmail.com>
2022-11-27 11:10:33 -0300

    section five

--- Front/ProEventosAPP/src/app/eventos/eventos.component.ts
+++ Front/ProEventosAPP/src/app/eventos/eventos.component.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public filtredEvents: any = [];
+
+  widthImg = 150;
+  marginImg = 5;
+  showImage = true;
+  private _filterList: string = '';
+
+  public get filterList() {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filtredEvents = this.filterList
+      ? this.filterEvents(this.filterList)
+      : this.eventos;
+  }
+
+  filterEvents(filterEvents: string): any {
+    filterEvents.toLocaleLowerCase();
+    return this.eventos.filter(
+      (e: { tema: string; local: string }) =>
+        e.tema.toLocaleLowerCase().indexOf(filterEvents) !== -1 ||
+        e.local.toLocaleLowerCase().indexOf(filterEvents) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,9 +41,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:7134/api/eventos').subscribe(
-      (res) => (this.eventos = res),
+      (res) => {
+        this.eventos = res;
+        this.filtredEvents = this.eventos;
+      },
+
       (err) => console.log(err)
     );
   }
