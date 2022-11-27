cc36b6408df0d5006ce46dc1b03d2db69b7a2478
Marcos César Domingues <dfenixweb@gmail.com>
2022-11-27 08:27:45 -0300

    add colapsad component

--- Front/ProEventosAPP/src/app/app.module.ts
+++ Front/ProEventosAPP/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
@@ -8,6 +7,7 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [AppComponent, EventosComponent, PalestrantesComponent, NavComponent],
@@ -16,6 +16,7 @@ import { NavComponent } from './nav/nav.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
