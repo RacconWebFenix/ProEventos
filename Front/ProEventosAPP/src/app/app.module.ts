53489bad6318ade28a12f5c93e4b999e920501b2
Marcos César Domingues <dfenixweb@gmail.com>
2022-12-01 12:13:47 -0300

    tootip and dropdown

--- Front/ProEventosAPP/src/app/app.module.ts
+++ Front/ProEventosAPP/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { EventoService } from './services/evento.service';
+
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 
 @NgModule({
   declarations: [
@@ -17,6 +24,7 @@ import { EventoService } from './services/evento.service';
     EventosComponent,
     PalestrantesComponent,
     NavComponent,
+    DateTimeFormatPipe,
   ],
   imports: [
     BrowserModule,
@@ -24,6 +32,8 @@ import { EventoService } from './services/evento.service';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
     FormsModule,
   ],
   providers: [EventoService],
